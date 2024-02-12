960ea448e8369a08bd1023077ae389ccdd368ac6
Aviij <avishkadd9@gmail.com>
2024-02-12 06:53:22 +0530

    project Completed

--- srcipt.js
+++ srcipt.js
@@ -122,7 +122,7 @@ const playNextSong = () => {
   }
 };
 
-const playPreviousSong = () => {
+const playPreviousSong = () =>{
    if (userData?.currentSong === null) return;
    else {
     const currentSongIndex = getCurrentSongIndex();
@@ -250,7 +250,22 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+  const currentSongIndex = getCurrentSongIndex();
+  const nextSongExists = userData?.songs[currentSongIndex + 1] !== undefined;
 
+    if (nextSongExists) {
+      playNextSong();
+    } else {
+      userData.currentSong = null;
+      userData.songCurrentTime = 0;  
+pauseSong();
+setPlayerDisplay();
+ highlightCurrentSong();
+ setPlayButtonAccessibleText();
+
+    }
+});
 
 userData?.songs.sort((a,b) => {
   if (a.title < b.title) {
@@ -264,6 +279,8 @@ userData?.songs.sort((a,b) => {
   return 0;
 });
 
+renderSongs(userData?.songs);
+setPlayButtonAccessibleText();
 renderSongs(userData?.songs);
 setPlayButtonAccessibleText();
 
